22b63c2d31d7cc6dd72ce60e06e40e5285b10b87
Edgar2000Xuc <117703302+Edgar2000Xuc@users.noreply.github.com>
2023-05-12 17:12:13 +0330

    Add files via upload

---
+++ Fecha.js
@@ -0,0 +1,10 @@
+//fechas de nacimiento
+// Fecha de hoy
+const hoy = new Date();
+
+// Fecha de tu nacimiento
+const fechaNacimiento = new Date("2000-02-11");
+
+const diaNacimiento = nacimiento.getDate()
+const mesNacimiento = nacimiento.getMonth() + 1
+const anyoNacimiento = nacimiento.getFullYear()
